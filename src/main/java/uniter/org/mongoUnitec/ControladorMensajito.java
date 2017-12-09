eec6ebf8e786c91bad60ef97ebd07c7cffe89455
Jorge <pichardojorge243@gmail.com>
2017-12-09 03:07:48 -0600

    Correcion

--- src/main/java/uniter/org/mongoUnitec/ControladorMensajito.java
+++ src/main/java/uniter/org/mongoUnitec/ControladorMensajito.java
@@ -10,28 +10,30 @@ import java.util.ArrayList;
 @RequestMapping("/api")
 @CrossOrigin
 public class ControladorMensajito {
-    @Autowired RepositorioMensajito repoMensa;
+    @Autowired
+    RepositorioMensajito repoMensa;
 
     //Aquí van las 5 operaciones básicas con la entidad Mensajito
 
     //Obtener todos
     @CrossOrigin
-    @RequestMapping(value="/mensajito", method=RequestMethod.GET, headers = {"Accept=application/json"})
-    public ArrayList<Mensajito> obetenerTodos()throws Exception{
+    @RequestMapping(value = "/mensajito", method = RequestMethod.GET, headers = {"Accept=application/json"})
+    public ArrayList<Mensajito> obetenerTodos() throws Exception {
         return (ArrayList<Mensajito>) repoMensa.findAll();
     }
 
     //Obtener por Id
     @CrossOrigin
-    @RequestMapping(value = "/mensajito/{id}", method = RequestMethod.GET, headers = {"Accept = application/json"})
-    public Mensajito obtenerPorId(@PathVariable String id) throws Exception{
+    @RequestMapping(value = "/mensajito/{id}", method = RequestMethod.GET, headers = {"Accept=application/json"})
+    public Mensajito obetenerPorId(@PathVariable String id) throws Exception {
         return repoMensa.findOne(id);
     }
 
+
     //Guardar Mensajito para clientes web y desktop
     @CrossOrigin
-    @RequestMapping(value = "/mensajito/{titulo}/{cuerpo}", method = RequestMethod.POST, headers = {"Accept = application/json"})
-    public Estatus guardarMensajito(@PathVariable String titulo, @PathVariable String cuerpo) throws Exception{
+    @RequestMapping(value = "/mensajito/{titulo}/{cuerpo}", method = RequestMethod.POST, headers = {"Accept=application/json"})
+    public Estatus guardarMensajito(@PathVariable String titulo, @PathVariable String cuerpo) throws Exception {
         repoMensa.save(new Mensajito(titulo, cuerpo));
         Estatus estatus = new Estatus(true);
         return estatus;
@@ -39,8 +41,8 @@ public class ControladorMensajito {
 
     //Guardar Mensajito, forma más pura y efectiva
     @CrossOrigin
-    @RequestMapping(value = "/mensajito", method = RequestMethod.POST, headers = {"Accept = application/json"})
-    public Estatus guardarMensajitoPuro(@RequestBody String json) throws Exception{
+    @RequestMapping(value = "/mensajito", method = RequestMethod.POST, headers = {"Accept=application/json"})
+    public Estatus guardarMensajitoPuro(@RequestBody String json) throws Exception {
         ObjectMapper maper = new ObjectMapper();
         Mensajito mensa = maper.readValue(json, Mensajito.class);
         repoMensa.save(mensa);
@@ -50,8 +52,8 @@ public class ControladorMensajito {
 
     //Actualizar Mensajito
     @CrossOrigin
-    @RequestMapping(value = "/mensajito", method = RequestMethod.PUT, headers = {"Accept = application/json"})
-    public Estatus actualizarMensajito(@RequestBody String json) throws Exception{
+    @RequestMapping(value = "/mensajito", method = RequestMethod.PUT, headers = {"Accept=application/json"})
+    public Estatus actualizarMensajito(@RequestBody String json) throws Exception {
         ObjectMapper maper = new ObjectMapper();
         Mensajito mensa = maper.readValue(json, Mensajito.class);
         Estatus es = new Estatus(true);
@@ -60,8 +62,8 @@ public class ControladorMensajito {
 
     //Eliminar Mensajito
     @CrossOrigin
-    @RequestMapping(value = "/mensajito/{id}", method = RequestMethod.DELETE, headers = {"Accept = application/json"})
-    public Estatus borrarMensajito(@PathVariable String id) throws Exception{
+    @RequestMapping(value = "/mensajito/{id}", method = RequestMethod.DELETE, headers = {"Accept=application/json"})
+    public Estatus borrarMensajito(@PathVariable String id) throws Exception {
         repoMensa.delete(id);
         Estatus es = new Estatus(true);
         return es;
